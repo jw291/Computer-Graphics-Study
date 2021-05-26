0077a67c7e2b6dc08e5e6a6fa4fb8189c83b515a
codejaebi <jw_29@naver.com>
2021-05-26 19:14:37 +0900

    Initial Commit

---
+++ Computer Graphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Computer Graphics
+//
+//  Created by jaewon na on 2021/05/26.
+//  Copyright © 2021 jaewon na. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
